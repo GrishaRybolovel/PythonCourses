55e887435ebd995684e79ff580174f1fadd60795
Григорий Рыболовлев <95378753+GrishaRybolovel@users.noreply.github.com>
2022-03-24 19:13:47 +0300

    Create Photozop.py

---
+++ Photozop.py
@@ -0,0 +1,34 @@
+from PIL import Image, ImageTk
+from tkinter import Frame, Canvas, Button, Tk, filedialog
+
+
+
+class Photoshop(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        self.createMenu()
+
+    def createMenu(self):
+        self.btnOpen = Button(text="Открыть", command=self.open)
+        self.btnOpen.place(x=30, y=200)
+        self.canvas = Canvas(self.parent, width=400, height=400, bg="green")
+        self.canvasImage = self.canvas.create_image(0, 0)
+        self.canvas.pack()
+
+    def open(self):
+        self.filename = filedialog.askopenfilename()
+
+        self.image = Image.open(self.filename)
+        self.photo = ImageTk.PhotoImage(self.image)
+        self.canvas.itemconfigure(self.canvasImage, image=self.photo, anchor="nw")
+
+
+
+
+
+root = Tk()
+root.title("Фотожоп")
+root.geometry("700x500")
+app = Photoshop(root)
+root.mainloop()
